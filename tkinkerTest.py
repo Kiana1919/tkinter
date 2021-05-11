a35736226ed9ff11f04f038f75e216096309bf1f
Kiana1919 <79869430+Kiana1919@users.noreply.github.com>
2021-05-11 11:03:35 -0600

    Add files via upload

---
+++ tkinkerTest.py
@@ -0,0 +1,69 @@
+from tkinter import *
+
+top = Tk()
+playlist = []
+    
+def printList():
+    print(playlist)
+
+def exporList():
+    with open('playlist.txt', 'w') as f:
+        for item in playlist:
+            f.write( "%s\n" % item)
+
+def clearWindow():
+    for widget in top.winfo_children():
+        widget.destroy()
+
+def mainMenu():
+    clearWindow()
+    LMain = Label(top, text = "Block 5 GUI Projects")
+    LMain.grid(column = 0, row = 1)
+    B1Main = Button(text = "Week 1", bg = "white", command = week1)
+    B1Main.grid(column = 0, row = 2)
+    BM2ain = Button(text = "Week 2", bg = "white")
+    B2Main.grid(column = 0, row = 3)
+    B3Main = Button(text = "Week 3", bg = "white")
+    B3Main.grid(column = 0, row = 4)
+
+def week1():
+    clearWindow()
+    def results():
+        result = E1.get()
+        playlist.append(result)
+        E1.delete(0, END)
+    
+    # This is a Label widget
+    L1 = Label(top, text="Playlist Generator")
+    L1.grid(column= 0, row= 1)
+
+    #This is an Entry widget
+    E1 = Entry(top, bd = 5)
+    E1.grid(column= 0, row= 2)
+
+    #This is a Button widget
+    B1 = Button(text = " + ", bg = "white", command = results)
+    B1.grid(column = 1, row= 2)
+
+    B2 = Button(text = " Print ", bg = "light blue", command = printList)
+    B2.grid(column = 2, row = 2)
+
+    B3 = Button(text= "Export List", bg = "#B4FFCD", command = ExportList)
+    B3.grid(column = 0, row = 3)
+
+    Bexit = Button(text= "Main Menu", bg = "light grey", command = mainMenu)
+    Bexit.grid(column = 1, row = 3)
+
+def week2():
+    clearWindow()
+    B1Week2 = Button()
+    L1Week2 = Label()
+    L2Week2 = Label()
+    L3Week2 = Label()
+    E1Week2 = Entry()
+    E2Week2 = Entry()
+
+if __name__ == "__main__":
+    mainMenu()
+    top.mainloop()
+
